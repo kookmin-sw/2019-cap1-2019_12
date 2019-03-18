4825b3b99ec728dc54028096ef66842ba3d2eb8b
Hyun Kyung <kyungkoh1328@naver.com>
2019-03-18 23:51:01 +0900

    result page update

---
+++ web/src/main/resources/templates/billboard/billboard.js/types/bb.d.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2017 ~ present NAVER Corp.
+ * billboard.js project is licensed under the MIT license
+ */
+import { Chart } from "./chart";
+import { ChartOptions } from "./options";
+
+export const bb: {
+	/**
+	 * Generate chart
+	 * @param options Chart generation options
+	 */
+	generate(options: ChartOptions): Chart;
+
+	/**
+	 * Version information
+	 */
+	version: string;
+
+	/**
+	 * An array containing instance created
+	 */
+	instance: Chart[];
+};
+
+export const dd: {
+	/**
+	 * Generate chart
+	 * @param options Chart generation options
+	 */
+	generate(options: ChartOptions): Chart;
+
+	/**
+	 * Version information
+	 */
+	version: string;
+
+	/**
+	 * An array containing instance created
+	 */
+	instance: Chart[];
+};
