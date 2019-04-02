e8a7d1440507a78b6d5102c7fde2f7f92ef06178
Hyun Kyung <kyungkoh1328@naver.com>
2019-04-03 01:42:33 +0900

    Setting DB

--- src/server_gradle/src/main/java/com/capstone/server/ServerApplication.java
+++ src/server_gradle/src/main/java/com/capstone/server/ServerApplication.java
@@ -1,13 +1,30 @@
 package com.capstone.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserMongoDBRepository repository ;
 
     public static void main(String[] args) {
+
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args)throws Exception{
+       // repository.deleteAll();
+
+        System.out.println("findall");
+        for (UserDTO user : repository.findAll()){
+            System.out.println(user);
+        }
+        System.out.println();
     }
 
 }
