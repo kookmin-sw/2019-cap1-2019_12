88962e3d1d2d4b4d4ab4dbc4dfb7ebfbafb35c24
­김혜인[컴퓨터공학부] <hyein15@kookmin.ac.kr>
2019-05-22 21:35:13 +0900

    사용자와 기업별 자소서간의 코사인 유사도 측정

--- src/nlp/module/predict_module.py
+++ src/nlp/module/predict_module.py
@@ -2,9 +2,11 @@ import pickle
 import joblib
 from konlpy.tag import Okt
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import csv
 import json
-
+import numpy as np
+import operator
 
 def openStopword():
     f = open('stopwords.csv', 'r', encoding='utf-8')
@@ -51,6 +53,30 @@ def SVCproba(model, text):
         result[keyword_eng[i]] = proba[i]
     return result
 
+def Cosine(company_dict, company, user_text_dict):
+    # cos = cosine_similarity(company_dict, user_text)
+    cosine_dict = {}
+    user_text = []
+
+    for key in user_text_dict.keys():
+        user_text.append(user_text_dict[key])
+
+    for i in range(len(company_dict)):
+        x = np.array(company_dict[company[i]]).reshape(1, -1)
+        y = np.array(user_text).reshape(1, -1)
+        cosine_dict[company[i]] = cosine_similarity(x, y)
+
+    sorted(cosine_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+    sorted_company = []
+
+    for key in cosine_dict.keys():
+        sorted_company.append(key)
+
+    top_company_name = sorted_company[0]
+    top_company_value = company_dict[top_company_name]
+
+    return (top_company_name, top_company_value, user_text)
 
 #   분석 후 json으로 저장
 def UserAnalysis(model, text):
@@ -69,11 +95,30 @@ if __name__ == "__main__":
     filename = 'SVC_PROB.joblib'
     svc_from_joblib = joblib.load(filename)
 
-#   keyword_names = ['글로벌역량', '능동', '도전', '성실', '소통', '인내심', '정직', '주인의식', '창의', '팀워크']
-#   job = ['architecture', 'IT', 'management', 'production', 'sales']
-#   company = ['samsung', 'hyundai', 'LG', 'SK', 'CJ']
+    keyword_names = ['글로벌역량', '능동', '도전', '성실', '소통', '인내심', '정직', '주인의식', '창의', '팀워크']
+    job = ['architecture', 'IT', 'management', 'production', 'sales']
+    company = ['samsung', 'hyundai', 'LG', 'SK', 'CJ']
+
+    company_dict = {
+        'samsung': [-0.8887642959439978, -0.2320497495038032, -0.3980158364286989, 0.07982216179178525,
+                    -0.4634689585655357, -1.4945375211875371, -1.5887466236154073, -0.3994969557289842,
+                    -1.9458319112295257, -0.6715375781390972],
+        'hyundai': [-0.8228359594421046, -0.4560047847307666, -0.4392581998869604, 0.05151212010714745,
+                    -0.37278093523020006, -1.441515818729987, -1.6143444997864906, -0.554804611382156,
+                    -1.831011837725321, -0.6144309645563442],
+        'LG': [-0.7620625480990436, -0.4474363044623253, -0.46674841323118554, 0.07333865957640484,
+               -0.43207572092095403, -1.4279317971574639, -1.4718943563369267, -0.6400563343545634, -1.7959020562081858,
+               -0.7120585840815354],
+        'SK': [-0.7200478445244407, -0.4990252126738345, -0.4127369498124192, 0.006192507305919692,
+               -0.41839920312881207, -1.4965564622475518, -1.4192891756133306, -0.552887345195595, -1.773997090857436,
+               -0.6549712334986287],
+        'CJ': [-0.7197954463771161, -0.3855618389458571, -0.5225478215844543, 0.010193007021379819, -0.2898697024011009,
+               -1.865131533958604, -1.4985025101392337, -0.6209880454739807, -1.7121484451524631, -0.5946502637415154]
+    } #테스트 데이터
+
 
     print(SVCproba(svc_from_joblib, "열정을 갖고 끊임없이 노력하는 사람"))
     print(SVCpredict(svc_from_joblib, "열정을 갖고 끊임없이 노력하는 사람"))
-
     print(UserAnalysis(svc_from_joblib, "열정을 갖고 끊임없이 노력하는 사람"))
+
+    print((Cosine(company_dict, company, SVCproba(svc_from_joblib,"열정을 갖고 끊임없이 노력하는 사람"))))
